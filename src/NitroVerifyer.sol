42f650d839ffa5844ecfba00cbc708305aa24b78
Ivan Temchenko <35359595i@proton.me>
2024-07-10 14:46:24 -0700

    feat: ownable inheritance

--- src/NitroVerifyer.sol
+++ src/NitroVerifyer.sol
@@ -2,8 +2,9 @@
 pragma solidity ^0.8.13;
 
 import {NitroProver} from "NitroProver/src/NitroProver.sol";
+import {Ownable} from "@openzeppelin-contracts-5.0.2/access/Ownable.sol";
 
-contract NitroVerifyer is NitroProver {
+contract NitroVerifyer is NitroProver, Ownable {
     mapping(bytes32 => bytes) PCRs;
     bytes32[] enclaves;
 
